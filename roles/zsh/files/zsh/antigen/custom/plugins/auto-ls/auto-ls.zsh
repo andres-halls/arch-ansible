776536f447f87dcc6d9b04c5b7bfb5db028b16a7
andres-liiver <andres.liiver@gmail.com>
2018-03-11 14:00:01 +0200

    Updated zsh auto-ls plugin

--- roles/zsh/files/zsh/antigen/custom/plugins/auto-ls/auto-ls.zsh
+++ roles/zsh/files/zsh/antigen/custom/plugins/auto-ls/auto-ls.zsh
@@ -1,13 +1,32 @@
 # vim: sw=2 ts=2 et!
+# set up default functions
+if [[ $#AUTO_LS_COMMANDS -eq 0 ]]; then
+  AUTO_LS_COMMANDS=(ls git-status)
+fi
+
+auto-ls-ls () {
+  ls -lh
+  [[ $AUTO_LS_NEWLINE != false ]] && echo ""
+}
+
+auto-ls-git-status () {
+  if [[ $(git rev-parse --is-inside-work-tree 2> /dev/null) == true ]]; then
+    git status
+  fi
+}
+
 auto-ls () {
   if [[ $#BUFFER -eq 0 ]]; then
     zle && echo ""
-    ls -lh
-    echo ""
-    if [[ $(git rev-parse --is-inside-work-tree 2> /dev/null) == true ]]; then
-      git status
-    fi
-
+    for cmd in $AUTO_LS_COMMANDS; do
+      # If we detect a command with full path, ex: /bin/ls execute it
+      if [[ $AUTO_LS_PATH != false && $cmd =~ '/' ]]; then
+        eval $cmd
+      else
+        # Otherwise run auto-ls function
+        auto-ls-$cmd
+      fi
+    done
     zle && zle redisplay
   else
     zle .$WIDGET
@@ -16,4 +35,7 @@ auto-ls () {
 
 zle -N auto-ls
 zle -N accept-line auto-ls
-add-zsh-hook chpwd auto-ls
+
+if [[ ${chpwd_functions[(I)auto-ls]} -eq 0 ]]; then
+  chpwd_functions+=(auto-ls)
+fi
